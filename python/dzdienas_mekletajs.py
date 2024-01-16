e2ee2fda1eaf52cf7e91ecc0b3c716953ab18943
sneeZ1E <112924573+sneeZ1E@users.noreply.github.com>
2024-01-16 16:27:04 +0200

    pabeidzam mekletaju
    W

--- python/dzdienas_mekletajs.py
+++ python/dzdienas_mekletajs.py
@@ -1,5 +1,6 @@
 def dienas_mekletajs (esosais_gads, esosais_menesis, esosais_datums, esosa_diena, dzimsanas_gads, dzimsanas_menesis, dzimsanas_datums):
-    menesu_dienu_skaits = [31, 28, 31, 30, 31, 30, 31, 31, 30, 31, 30, 31]
+    menesu_dienu_skaits = [31, 31, 28, 31, 30, 31, 30, 31, 31, 30, 31, 30, 31]
+    dienu_nosaukumi = ["Nekas", "pirmdiena", "otrdiena", "trešdiena", "ceturtdiena", "piektdiena", "sestdiena", "svētdiena"]
     
     #Tiek skaitits cik dienas pagajusas
     #Tiek parbaudits vai sogad jau ir bijusi dzimsanas diena
@@ -33,11 +34,48 @@ def dienas_mekletajs (esosais_gads, esosais_menesis, esosais_datums, esosa_diena
 
     pagajusas_dienas = pagajusas_dienas+garie_gadi
 
+    #Tiek skaitits cik pilni menesi pagajusi kops pedejas dzd
+    if esosais_menesis>=dzimsanas_menesis:
+        pilni_menesi = esosais_menesis-dzimsanas_menesis
 
-    #Tiek skaitits cik pilni menesi pagajusi kops dzd
+    else:
+        pilni_menesi = esosais_menesis+12-dzimsanas_menesis
+
+    if vai_datums_jau_pagajis(1, esosais_datums, 1, dzimsanas_datums) == False:
+        pilni_menesi -= 1
+
+    dienas_menesos = 0
+
+
+    menesis = dzimsanas_menesis
+    while menesis != esosais_menesis:
+        dienas_menesos += menesu_dienu_skaits[menesis]
+        menesis += 1
+        if menesis == 13:
+            menesis=1
+    
+    pagajusas_dienas += dienas_menesos
+
     #Tiek skaitits cik dienas pari pilniem menesiem ir pagajusas
 
-    return "harosh"
+    if esosais_datums>=dzimsanas_datums:
+        ekstra_dienas = esosais_datums-dzimsanas_datums
+    else:
+        ekstra_dienas = esosais_datums + menesu_dienu_skaits[esosais_menesis-1] - dzimsanas_datums
+
+    pagajusas_dienas += ekstra_dienas
+    
+    print("Kopš dzimšanas dienas ir pagājušas: ", pagajusas_dienas, "dienas.")
+    
+    dienu_atlikums = pagajusas_dienas %7
+    dzimsanas_diena = esosa_diena-dienu_atlikums
+
+    if dzimsanas_diena<=0:
+        dzimsanas_diena +=7
+
+    print("Jums ir ", pagajusie_gadi, " gadi, ", pilni_menesi, " mēneši un ", ekstra_dienas, "dienas.")
+
+    return dienu_nosaukumi[dzimsanas_diena]
 
 
 def vai_datums_jau_pagajis(esosais_menesis, esosais_datums, dzimsanas_menesis, dzimsanas_datums):
@@ -48,3 +86,20 @@ def vai_datums_jau_pagajis(esosais_menesis, esosais_datums, dzimsanas_menesis, d
     if esosais_datums<dzimsanas_datums:
         return False
     return True
+
+
+dzimsanas_gads = int(input("Lūdzu ievadiet savu dzimšanas dienas gadu:"))
+
+dzimsanas_menesis = int(input("Lūdzu ievadiet savu dzimšanas dienas mēnesi (1-12):"))
+
+dzimsanas_datums = int(input("Lūdzu ievadiet savu dzimšanas dienas datumu (1-31):"))
+
+esosais_gads = int(input("Lūdzu ievadiet pašreizējo gadu:"))
+
+esosais_menesis = int(input("Lūdzu ievadiet pašreizējo mēnesi (1-12):"))
+
+esosais_datums = int(input("Lūdzu ievadiet šodienas datumu (1-31):"))
+
+esosa_nedelas_diena = int(input("Lūdzu ievadiet pašreizējo nedēļas dienu (1-7):"))
+
+print(dienas_mekletajs(esosais_gads, esosais_menesis, esosais_datums, esosa_nedelas_diena, dzimsanas_gads, dzimsanas_menesis, dzimsanas_datums))
