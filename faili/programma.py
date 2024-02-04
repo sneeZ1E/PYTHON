fa3c175bd2e78ba59f98f74ee5d3c0124e6f0d15
sneeZ1E <112924573+sneeZ1E@users.noreply.github.com>
2024-02-04 21:08:19 +0200

    VESTULES

--- faili/programma.py
+++ faili/programma.py
@@ -1,3 +1,5 @@
+
+
 def ierakstit(teksts, faila_nosaukums):
     fails = open(faila_nosaukums, "w", encoding='utf-8')
     fails.write(teksts)
@@ -13,8 +15,27 @@ def nolasit(faila_nosaukums):
         rindas = fails.readlines()
     return rindas
 
+def uzrakstit_vestuli(vards, uzvards, dzimums, vecums, index):
+    if dzimums == "s":
+        galotne1 = "a"
+        galotne2 = "usi"
+    else:
+        galotne1 = "s"
+        galotne2 = "is"
 
+    fails = open("./vestules/vestule{}.txt".format(index), "w", encoding="utf-8")
+    fails.write("Sveik{}, {} {}! \n".format(galotne1, vards, uzvards))
+    fails.write("Jūs esat laimēj{} {}0 EUR!".format(galotne2, vecums)) 
+    
+    return
 
+def apstradat_datus(dati):
+    dati = dati.split()
+    if dati[4] == "Sieviete":
+        dzimums = "s"
+    else:
+        dzimums = "v"
+    return dati[0], dati[1], dati[3][:-1], dati[4][:-1], dzimums
 
 vardi = ["Anna", "Maija", "Jānis", "Kaspars"]
 uzvardi = ["Bērziņa", "Paija", "Ozols", "Kasprets"]
@@ -31,9 +52,19 @@ for i in range(len(vardi) ):
     teksts = "{} {} - {}, {}, {} \n".format(vardi[i], uzvardi[i], vecums[i], rakstamais, statuss[i])
     pierakstit(teksts, "faili/cilveki.txt")
 
+
+cilveki = nolasit("faili/cilveki.txt")
+index = 0
+for cilveks in cilveki:
+    cilveka_dati = apstradat_datus(cilveks)
+    uzrakstit_vestuli(cilveka_dati, index)
+    index+=1
+    
+
+
 # izveidojas fails kur katra rindina ir vards, uzvards - vecums
 # Anna Bērziņa - 23, sieviete
-
+# Sveiks/-a, ....! Tu esi laimējis/-usi .... Euro!
 
 
 # print(nolasit("faili/teksts.txt"))
